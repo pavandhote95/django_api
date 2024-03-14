4f3776406091407259551dd831fc5c82453218c7
pavan <pavandhote95@gmail.com>
2024-03-14 09:03:22 +0530

    added cors origin

--- banner_project/settings.py
+++ banner_project/settings.py
@@ -55,11 +55,12 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+ 
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 ]
 
@@ -146,3 +147,4 @@ REST_FRAMEWORK = {
     ]
 }
 
+CORS_ALLOWED_ORIGINS_ALL = 
